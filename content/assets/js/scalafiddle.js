cc05914a363e181cd84b2f33722102e38b851f7e
PJ Fanning <pjfanning@users.noreply.github.com>
2023-01-15 21:31:08 +0100

    init

---
+++ content/assets/js/scalafiddle.js
@@ -0,0 +1,8 @@
+window.scalaFiddleTemplates = {
+  "Akka": {
+    pre: "// $FiddleDependency org.akka-js %%% akkajsactor % 2.2.6.1 \n" +
+    "// $FiddleDependency org.akka-js %%% akkajsactorstream % 2.2.6.1 \n" +
+    "// $FiddleDependency org.akka-js %%% akkajsactortyped % 2.2.6.1 \n",
+    post: ""
+  }
+}
